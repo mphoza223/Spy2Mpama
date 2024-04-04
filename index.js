9f47704c39a2be7de064ed25c193277e844007b5
Mpho_Kutoane <mphothedev@gmail.com>
2024-04-04 19:00:11 +0200

    docs(guid/cli): clarify example description

---
+++ index.js
@@ -0,0 +1,47 @@
+const localRepo = __dirname + '/../spy2mpama';
+const simpleGit = require("simple-git");
+const git = simpleGit();
+var fs = require('fs')
+const gitFilePath = __dirname + "/../spy2mpama/www/gitNotes.txt";
+const remoteRepo = 'https://github.com/mphoza223/Spy2Mpama.git';
+
+const messages = require("./msg.js").msg_arr;
+const runs = Math.floor(Math.random() * 4) + 1; //decide how many commits we're making
+
+function getMessage(){
+	let rnd = Math.floor(Math.random() * (messages.length));
+	return messages[rnd];
+}
+
+
+
+// console.log(gitFilePath)
+
+
+fs.readFile(gitFilePath, 'utf8', function(err, data){
+var newValue = data.replace('', '\n');
+	
+	fs.writeFile(gitFilePath, newValue, async function(){
+
+		try{
+
+				// await git.checkout("main");
+				// const branch = await git.branch();
+
+				// // logs "test-branch"
+				// console.log(branch.current)
+
+			     await git.pull(localRepo,'main')
+			     await git.add('.')
+			     await git.commit(getMessage())
+			     await git.addRemote('origin', remoteRepo)
+			     await git.push(['-u', 'origin', 'main'], () => console.log('push successful'));
+
+			}catch(error){
+			console.log(error)
+		}
+		
+	});
+});
+
+
---
+++ msg.js
@@ -0,0 +1,16 @@
+
+const messages = {
+msg_arr: [
+	'refactor player mode',
+	'chore(benchpress): add ngRepeat animation benchmark',
+	'style(css ionic) separate selectors and declarations by new lines',
+	'docs(guid/cli): clarify example description',
+	'chore: fix UCD extractoin for Node 12.x',
+	'docs(*): optimize images',
+	'chore: suppress verbose log output and allow error logging',
+	'creating logs/ and tmp/ dirs',
+	'chore(scripts): use https for git repo urls',
+	'fix(required): correctly validate required on non-input elements']
+}
+
+module.exports = messages;
