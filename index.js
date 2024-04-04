8270c796f42380e6770d3f04a825388ab05b3ffe
Mpho_Kutoane <mphothedev@gmail.com>
2024-04-04 19:12:34 +0200

    chore: fix UCD extractoin for Node 12.x

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const localRepo = __dirname + '/../spy2mpama';
+
 const simpleGit = require("simple-git");
 const git = simpleGit();
 var fs = require('fs')
 const gitFilePath = __dirname + "/../spy2mpama/www/gitNotes.txt";
 const remoteRepo = 'https://github.com/mphoza223/Spy2Mpama.git';
-
+const localRepo = __dirname + '/../spy2mpama';
 const messages = require("./msg.js").msg_arr;
 const runs = Math.floor(Math.random() * 4) + 1; //decide how many commits we're making
 
@@ -13,10 +13,7 @@ function getMessage(){
 	return messages[rnd];
 }
 
-
-
 // console.log(gitFilePath)
-
 
 fs.readFile(gitFilePath, 'utf8', function(err, data){
 var newValue = data.replace('', '\n');
@@ -25,17 +22,12 @@ var newValue = data.replace('', '\n');
 		const msg = getMessage();
 		try{
 
-				// await git.checkout("main");
-				const branch = await git.branch('-M', 'main');
-
-				// // logs "test-branch"
-				// console.log(branch.current)
-
-			     await git.pull(localRepo,'main')
-			     await git.add('.')
-			     await git.commit(msg)
-			     // await git.addRemote('origin', remoteRepo)
-			     await git.push(['-u', 'origin', branch.current], () => console.log('push successful :', msg));
+		 	 const branch = await git.branch('-M', 'main');
+		     await git.pull(localRepo,'main')
+		     await git.add('.')
+		     await git.commit(msg)
+		     // await git.addRemote('origin', remoteRepo)
+		     await git.push(['-u', 'origin', branch.current], () => console.log('push successful :', msg));
 
 			}catch(error){
 			console.log(error)
